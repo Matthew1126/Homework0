a9b886c7780cf8301e3a5cbac588b7ebaf2c7f6b
Matthew1126 <muo53@psu.edu>
2020-08-26 19:46:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+celcius = input('Enter temperature in celcius: ')
+celcius = int(celcius)
+fahrenheit = int(celcius * 1.8 + 32)
+print( str(celcius) + "° in Celcius is equivalent to " + str(fahrenheit) + "° fahrenheit")
